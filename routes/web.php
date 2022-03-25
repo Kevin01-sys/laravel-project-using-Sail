82383dc01c4d3b85c7308023267e1a3e5c0ee9aa
Kevin Duoc <kevinchocano@gmail.com>
2022-03-25 11:39:57 -0300

    in web.php you learned how to use the routes in the basic way

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Bienvenido a la página principal ";
+});
+Route::get('cursos', function () {
+    return "Bienvenido a la página de cursos ";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta página podrás crear un curso";
+});
+
+Route::get('cursos/{curso}', function ($curso) {
+    return  "Bienvenido al curso:  $curso";
+});
+
+/*Route::get('cursos/{id}', function ($id) {
+    return  "Bienvenido al $id";
+});*/
+
+// categoria? se deja con el signo porque es opcional, y se debe dejar null
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria){
+        return "Bienvenido al curso $curso, de la categoría $categoria";
+    } else 
+    {
+        return "Bienvenido al curso: $curso";
+    }
+
 });
