25958cb99436ef89af8b775200461fd4453c38a1
Kevin Duoc <kevinchocano@gmail.com>
2022-04-01 12:43:48 -0300

    Integrating Seeders in Laravel

---
+++ database/migrations/2022_04_01_153055_add_categoria_to_cursos_table.php
@@ -0,0 +1,32 @@
+<?php
+// php artisan make:migration add_categoria_to_cursos_table # command used to created this file
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cursos', function (Blueprint $table) {
+            $table->string('categoria')->after('descripcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cursos', function (Blueprint $table) {
+            $table->dropColumn('categoria');
+        });
+    }
+};
