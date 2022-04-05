52ae16bbd7b4b83d34bbdd35348389469965bac7
Kevin Duoc <kevinchocano@gmail.com>
2022-04-05 11:43:35 -0400

    accessors and mutators (getters and setters) in Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,30 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            // arrow function
+            get: fn($value) => ucwords($value),
+            set: fn($value) => strtolower($value)
+
+            /*get: function($value){
+                return ucwords($value);
+            },
+
+            set: function($value){
+                return strtolower($value);
+            }*/
+
+
+        );
+    }
+    /* accessors and mutators for older versions of Laravel
+    public function getNameAttribute($value){
+        return ucwords($value);
+    }
+    public function setNameAttribute($value){
+        return strtolower($value);
+    }*/
 }
