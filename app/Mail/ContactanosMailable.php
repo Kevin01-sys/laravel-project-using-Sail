6ba5bc35846ead895f12e66773b996f85eb87b47
Kevin Chocano <kevinchocano@gmail.com>
2022-04-11 12:32:37 -0400

    25 How to send emails with Laravel

---
+++ app/Mail/ContactanosMailable.php
@@ -0,0 +1,36 @@
+<?php
+/* php artisan make:mail ContactanosMailable */
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactanosMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+    
+    #subject in email
+    public $subject = "Información de contacto";
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.contactanos');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+use App\Mail\ContactanosMailable;
+use Illuminate\Support\Facades\Mail;
+
 /*
 |--------------------------------------------------------------------------
 	@@ -13,33 +15,11 @@
@@ -13,20 +16,10 @@ use App\Http\Controllers\HomeController;
 Route::get('/', HomeController::class)->name('home');
 Route::resource('cursos', CursoController::class);
 Route::view('nosotros','nosotros')->name('nosotros');
-/* names() -> rename the routes | parameters() -> rename the variables
- Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos'); */
-
-/*Route::controller(CursoController::class)->group(function(){
-    Route::get('cursos','index')->name('cursos.index');
-    Route::get('cursos/create','create')->name('cursos.create');
-    Route::post('cursos','store')->name('cursos.store');
-    categoria? se deja con el signo porque es opcional, y se debe dejar null
-    Route::get('cursos/{id}','show')->name('cursos.show');
-    Route::get('cursos/{curso}/edit','edit')->name('cursos.edit');
-    Route::put('cursos/{curso}','update')->name('cursos.update');
-    Route::delete('cursos/{curso}','destroy')->name('cursos.destroy');
-    //Route::get('cursos/{curso}/{categoria?}','show');
-});*/
-
+Route::get('contactanos', function(){
+    $correo = new ContactanosMailable;
+    Mail::to('test@mailhog.local')->send($correo);
+    return "mensaje enviado";
+});
 
  
